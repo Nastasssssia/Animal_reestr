7497c8a7fc7ec3ca0d4a40ccb0bcc215f93d070a
Nastassia <anastasia.goroninova@icloud.com>
2024-05-26 18:15:53 +0300

    creat abstract class

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Animal {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+// Базовый класс Животные
+abstract class Animal {
+    protected String name;
+    protected Date birthDate;
+    protected List<String> commands;
+    private static int animalCount = 0;
+
+    public Animal(String name, Date birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.commands = new ArrayList<>();
+        animalCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public List<String> getCommands() {
+        return commands;
+    }
+
+    public void addCommand(String command) {
+        commands.add(command);
+    }
+
+    public static int getAnimalCount() {
+        return animalCount;
+    }
 }
+
+
